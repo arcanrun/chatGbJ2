781f72050d94bd6316f51787d22655f7d750ee1e
arcanrun <arcan.run@gmail.com>
2020-03-09 12:49:57 +0300

    lesson06.4

--- chat-server/src/ru/gb/jt/chat/server/core/ChatServer.java
+++ chat-server/src/ru/gb/jt/chat/server/core/ChatServer.java
@@ -6,11 +6,13 @@ import ru.gb.jt.network.ServerSocketThreadListener;
 import ru.gb.jt.network.SocketThread;
 import ru.gb.jt.network.SocketThreadListener;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Vector;
+import java.util.logging.*;
 
 public class ChatServer implements ServerSocketThreadListener, SocketThreadListener {
 
@@ -18,9 +20,19 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
     private ServerSocketThread server;
     private final DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss: ");
     private Vector<SocketThread> clients = new Vector<>();
+    private static final Logger logger = Logger.getLogger(ChatServer.class.getName());
 
     public ChatServer(ChatServerListener listener) {
         this.listener = listener;
+        try{
+
+        Handler handler = new FileHandler("server.log",10*1024,20 );
+        handler.setFormatter(new XMLFormatter());
+        handler.setLevel(Level.ALL);
+        logger.addHandler(handler);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void start(int port) {
@@ -52,12 +64,14 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
     @Override
     public void onServerStart(ServerSocketThread thread) {
         putLog("Server thread started");
+        logger.log(Level.INFO, "Server thread started");
         SqlClient.connect();
     }
 
     @Override
     public void onServerStop(ServerSocketThread thread) {
         putLog("Server thread stopped");
+        logger.log(Level.INFO, "Server thread stopped");
         SqlClient.disconnect();
         for (int i = 0; i < clients.size(); i++) {
             clients.get(i).close();
@@ -68,18 +82,21 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
     @Override
     public void onServerSocketCreated(ServerSocketThread thread, ServerSocket server) {
         putLog("Server socket created");
+        logger.log(Level.INFO, "Server socket created");
 
     }
 
     @Override
     public void onServerTimeout(ServerSocketThread thread, ServerSocket server) {
 //        putLog("Server timeout");
+        logger.log(Level.INFO, "Server timeout");
 
     }
 
     @Override
     public void onSocketAccepted(ServerSocketThread thread, ServerSocket server, Socket socket) {
         putLog("Client connected");
+        logger.log(Level.INFO, "Client connected");
         String name = "SocketThread " + socket.getInetAddress() + ":" + socket.getPort();
         new ClientThread(this, name, socket);
 
@@ -87,9 +104,11 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
 
     @Override
     public void onServerException(ServerSocketThread thread, Throwable exception) {
+
+        logger.log(Level.WARNING, exception.toString());
+
         exception.printStackTrace();
     }
-
     /**
      * Socket methods
      *
@@ -98,12 +117,13 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
     @Override
     public synchronized void onSocketStart(SocketThread thread, Socket socket) {
         putLog("Socket created");
-
+        logger.log(Level.INFO, "Socket created");
     }
 
     @Override
     public synchronized void onSocketStop(SocketThread thread) {
         ClientThread client = (ClientThread) thread;
+        logger.log(Level.INFO, "Client:" + thread + "has left the session");
         clients.remove(thread);
         if (client.isAuthorized() && !client.isReconnecting() ) {
             sendToAuthClients(Library.getTypeBroadcast("Server",
@@ -114,6 +134,7 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
 
     @Override
     public synchronized void onSocketReady(SocketThread thread, Socket socket) {
+        logger.log(Level.INFO, "Client:" + thread + "has been connected");
         clients.add(thread);
     }
 
